35872a9e330e68233dd14f78e9e4e946ec3b1ff1
Joey Gale <joey.gale@my.simpson.edu>
2018-10-23 10:20:05 -0500

    fixed issue in breakfast tab

--- public_html/breakfast.php
+++ public_html/breakfast.php
@@ -4,9 +4,9 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" type="text/css" href="css/css_test_m.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/lumen/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/css_test_m.css">
 		
 		<title>Breakfast</title>
 	</head>
