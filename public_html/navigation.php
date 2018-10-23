b5aa0d34e5d4719f38e497865476c4440e1cc321
Joey Gale <joey.gale@my.simpson.edu>
2018-10-23 10:07:39 -0500

    Fixed navigation

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -11,7 +11,7 @@
 				<a class="nav-link" href="index.php">Home</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="about.php">About </a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="housing.php">Our Rooms</a>
