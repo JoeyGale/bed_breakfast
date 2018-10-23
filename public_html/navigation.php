1327da75c599426cc747afaad9c4fb059a79b7e2
Joey Gale <joey.gale@my.simpson.edu>
2018-10-23 10:16:59 -0500

    Changed nav so booking isn't active

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -19,7 +19,7 @@
 			<li class="nav-item">
 				<a class="nav-link" href="breakfast.php">Breakfast</a>
 			</li>
-			<li class="nav-item active">
+			<li class="nav-item">
 				<a class="nav-link" href="booking.php">Booking</a>
 			</li>
 			<li class="nav-item">
